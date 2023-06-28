199282732752074a9185196897a79ec39c6083cf
Cesar <CesarCordova.developer@gmail.com>
2023-06-27 18:46:30 -0700

    primer commit del projecto

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { CButton } from '@coreui/react'
+
+function App() {
+  return (
+    <>
+      <div className='bg-green-300	'>
+      <CButton color="success">Primary</CButton>
+        <h1 className='text-4xl text-center'>Hello World</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import '@coreui/coreui/dist/css/coreui.min.css'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
